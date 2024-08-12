1134b5cc6b474ed728a4fc73fffaa9c1df8e2cff
Dashka-Cherepashka <92571932+Dashka-Cherepashka@users.noreply.github.com>
2024-08-13 01:27:59 +0300

    Добавлен тестовый интерфейс
    Нуждается в доработке

--- WinFormsApp/Form.Designer.cs
+++ WinFormsApp/Form.Designer.cs
@@ -22,46 +22,217 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
+            components = new System.ComponentModel.Container();
+            var resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
             groupBox1 = new GroupBox();
+            time = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            groupBox2 = new GroupBox();
+            label3 = new Label();
+            label1 = new Label();
+            inputHeight = new TextBox();
+            inputMines = new TextBox();
+            label2 = new Label();
+            inputWidth = new TextBox();
             NewGame = new Button();
+            comboBox1 = new ComboBox();
+            field = new TableLayoutPanel();
+            timer = new System.Windows.Forms.Timer(components);
             groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(time);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(groupBox2);
             groupBox1.Controls.Add(NewGame);
-            groupBox1.Location = new Point(12, 4);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.FlatStyle = FlatStyle.Flat;
+            groupBox1.Location = new Point(12, 0);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(522, 100);
+            groupBox1.Size = new Size(650, 100);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             // 
+            // time
+            // 
+            time.AutoSize = true;
+            time.Font = new Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            time.Location = new Point(463, 55);
+            time.Name = "time";
+            time.Size = new Size(73, 20);
+            time.TabIndex = 7;
+            time.Text = "00:00:00";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label5.Location = new Point(522, 23);
+            label5.Name = "label5";
+            label5.Size = new Size(16, 17);
+            label5.TabIndex = 6;
+            label5.Text = "0";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label4.Location = new Point(460, 23);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 17);
+            label4.TabIndex = 5;
+            label4.Text = "Бомбы:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(inputHeight);
+            groupBox2.Controls.Add(inputMines);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(inputWidth);
+            groupBox2.Location = new Point(140, 0);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(128, 100);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Visible = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 75);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Мины:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Высота:";
+            // 
+            // inputHeight
+            // 
+            inputHeight.Location = new Point(56, 17);
+            inputHeight.Name = "inputHeight";
+            inputHeight.Size = new Size(56, 23);
+            inputHeight.TabIndex = 2;
+            // 
+            // inputMines
+            // 
+            inputMines.Location = new Point(56, 71);
+            inputMines.Name = "inputMines";
+            inputMines.Size = new Size(56, 23);
+            inputMines.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Ширина:";
+            // 
+            // inputWidth
+            // 
+            inputWidth.Location = new Point(56, 44);
+            inputWidth.Name = "inputWidth";
+            inputWidth.Size = new Size(56, 23);
+            inputWidth.TabIndex = 3;
+            // 
             // NewGame
             // 
-            NewGame.Location = new Point(244, 22);
+            NewGame.FlatAppearance.BorderSize = 0;
+            NewGame.FlatStyle = FlatStyle.Flat;
+            NewGame.Image = Properties.Resources.start_button;
+            NewGame.Location = new Point(285, 10);
             NewGame.Name = "NewGame";
-            NewGame.Size = new Size(36, 29);
+            NewGame.RightToLeft = RightToLeft.No;
+            NewGame.Size = new Size(92, 86);
             NewGame.TabIndex = 0;
-            NewGame.Text = "X";
             NewGame.UseVisualStyleBackColor = true;
+            NewGame.Click += NewGame_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Простой", "Средний", "Сложный", "Задать вручную" });
+            comboBox1.Location = new Point(8, 16);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(117, 23);
+            comboBox1.TabIndex = 1;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // field
+            // 
+            field.Anchor = AnchorStyles.None;
+            field.ColumnCount = 1;
+            field.ColumnStyles.Add(new ColumnStyle());
+            field.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            field.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            field.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
+            field.Location = new Point(12, 106);
+            field.Name = "field";
+            field.RowCount = 2;
+            field.RowStyles.Add(new RowStyle());
+            field.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            field.Size = new Size(650, 550);
+            field.TabIndex = 1;
+            // 
+            // timer
+            // 
+            timer.Interval = 1000;
+            timer.Tick += timer1_Tick;
             // 
             // Form
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(546, 581);
+            ClientSize = new Size(674, 671);
+            Controls.Add(field);
             Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Сапёр 2.0";
+            Load += Form_Load;
             groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private GroupBox groupBox1;
+        private TableLayoutPanel field;
+        private ComboBox comboBox1;
+        private GroupBox groupBox2;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox inputMines;
+        private TextBox inputWidth;
+        private TextBox inputHeight;
+        private System.Windows.Forms.Timer timer;
+        private Label label4;
+        private Label label5;
         private Button NewGame;
+        private Label time;
     }
 }
--- WinFormsApp/Form.cs
+++ WinFormsApp/Form.cs
@@ -1,7 +1,230 @@
+using Core;
+using System;
+using System.Media;
+using WinFormsApp.Properties;
 namespace WinFormsApp {
-    public partial class Form : System.Windows.Forms.Form {
-        public Form() {
+    public partial class Form : System.Windows.Forms.Form 
+    {
+        SoundPlayer start;
+        SoundPlayer flag = new SoundPlayer("tapok.wav");
+        SoundPlayer babah = new SoundPlayer("davai-po-novoi-misha.wav");
+        SoundPlayer win = new SoundPlayer("win.wav");
+        Minesweeper minesweeper;
+
+        int height;
+        int width;
+        int mines;
+        int countCells;
+        bool tm;
+        TimeSpan elapsedTime;
+        public Form() 
+        {
             InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            elapsedTime = new TimeSpan(0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) 
+        {
+            elapsedTime = elapsedTime.Add(TimeSpan.FromSeconds(1));
+            time.Text = elapsedTime.ToString(@"hh\:mm\:ss");
+        }
+
+        private void MoveToCenterScreen() 
+        {
+            var screen = Screen.FromControl(this);
+            this.Top = screen.Bounds.Height / 2 - this.Height / 2;
+            this.Left = screen.Bounds.Width / 2 - this.Bounds.Width / 2;
+        }
+
+        private void DrawField(int height, int width) 
+        {
+            field.Controls.Clear();
+            field.RowStyles.Clear();
+            field.ColumnStyles.Clear();
+            field.RowCount = height;
+            field.ColumnCount = width;
+
+            int buttonSize = 40;
+
+            for(int i = 0; i < width; i++) {
+                field.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, buttonSize));
+                for(int j = 0; j < height; j++) {
+                    if(i == 0) field.RowStyles.Add(new RowStyle(SizeType.Absolute, buttonSize));
+                    Button b = new Button();
+                    b.Size = new Size(buttonSize, buttonSize);
+                    b.FlatStyle = FlatStyle.Flat;
+                    b.Click += new EventHandler(Cell_Click!);
+                    b.MouseDown += new MouseEventHandler(Cell_MouseDown!);
+                    field.Controls.Add(b, i, j);
+                }
+            }
+            ReturnOldSize();
+            field.Size = new Size(buttonSize * width, buttonSize * height);
+            if(field.Width > this.ClientSize.Width || field.Height > this.ClientSize.Height) {
+                this.ClientSize = new Size(Math.Max(this.ClientSize.Width, field.Width) + 10, Math.Max(this.ClientSize.Height, field.Height) + groupBox1.Height);
+            }
+
+            field.Location = new Point((this.ClientSize.Width - field.Width) / 2, (this.ClientSize.Height + groupBox1.Height - field.Height) / 2);
+            groupBox1.Location = new Point((this.ClientSize.Width - groupBox1.Width) / 2, 10);
+        }
+
+        private void Set(Result r) 
+        {
+            foreach(Result? item in r.results) 
+            {
+                if(item is not null) 
+                {
+                    Button btn = (Button)field.GetControlFromPosition(item.Y, item.X)!;
+                    btn!.MouseDown -= Cell_MouseDown!;
+                    countCells--;
+                    if(item.IsBomb) btn.Image = Resources.bomb;
+                    else if(item.NumberOfBombs == 0) { btn.Image = null; btn.BackColor = Color.Gray; }
+                    else btn.Text = item.NumberOfBombs.ToString();
+                    Set(item);
+                }
+            }
+        }
+
+        private void Cell_Click(object sender, EventArgs e) 
+        {
+            if (!tm) { timer.Start(); tm = true; }
+            Button? b = sender as Button;
+            TableLayoutPanelCellPosition position = field.GetPositionFromControl(b);
+            Result res = minesweeper.CheckCell(position.Column, position.Row);
+
+            if(res.IsBomb) 
+            {
+                b!.Image = Resources.bomb;
+                NewGame.Image = Resources.nothehe;
+                babah.Play();
+                timer.Stop();
+                elapsedTime = new TimeSpan(0);
+                tm = false;
+                for(int row = 0; row < height; row++) 
+                {
+                    for(int col = 0; col < width; col++) 
+                    {
+                        Button btn = (Button)field.GetControlFromPosition(col, row)!;
+                        btn.Click -= Cell_Click!;
+                        if(minesweeper.Bombs[row * width + col]) btn.Image = Resources.bomb;
+                    }
+                }
+            }
+            
+            if (res.NumberOfBombs == 0) 
+            {
+                b!.BackColor = Color.Gray;
+                Set(res);
+            }
+            else b!.Text = res.NumberOfBombs.ToString();
+            b.Click -= Cell_Click!;
+            b.MouseDown -= Cell_MouseDown!;
+
+            countCells--;
+            if(countCells == mines) 
+            {
+                for(int row = 0; row < height; row++) {
+                    for(int col = 0; col < width; col++) {
+                        Button btn = (Button)field.GetControlFromPosition(col, row)!;
+                        btn.Click -= Cell_Click!;
+                        if(minesweeper.Bombs[row * width + col]) btn.Image = Resources.tapok;
+                    }
+                }
+                label5.Text = "0";
+                timer.Stop();
+                elapsedTime = new TimeSpan(0);
+                tm = false;
+                win.Play();
+                MessageBox.Show("УРА, ПОБЕДА!");
+            }
+        }
+
+        void Cell_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e) 
+        {
+            Button? b = sender as Button;
+            if (e.Button == MouseButtons.Right) 
+            {
+                if(b!.Image != null) 
+                {
+                    b.Image = null;
+                    b.FlatStyle = FlatStyle.Flat;
+                    label5.Text = (Convert.ToInt32(label5.Text) + 1).ToString();
+                } 
+                else 
+                {
+                    b.Image = Resources.tapok;
+                    b.FlatStyle = FlatStyle.Flat;
+                    flag.Play();
+                    label5.Text = (Convert.ToInt32(label5.Text) - 1).ToString();
+                    b.FlatStyle = FlatStyle.Flat;
+                    b.FlatStyle = FlatStyle.Standard;
+                }
+            }
+        }
+    
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            tm = false;
+            elapsedTime = new TimeSpan(0);
+            time.Text = elapsedTime.ToString(@"hh\:mm\:ss");
+
+            NewGame.Image = Resources.hehe;
+            start = new SoundPlayer("startuem.wav");
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    height = width = 9;
+                    mines = 10;
+                    break;
+                case 1:
+                    height = width = 16;
+                    mines = 40;
+                    start = new SoundPlayer("pognali.wav");
+                    break;
+                case 2:
+                    height = 16;
+                    width = 30;
+                    mines = 99;
+                    start = new SoundPlayer("kazahstan.wav");
+                    break;
+                case 3:
+                    try
+                    {
+                        height = Convert.ToInt32(inputHeight.Text);
+                        width = Convert.ToInt32(inputWidth.Text);
+                        mines = Convert.ToInt32(inputMines.Text);
+                    }
+                    catch { MessageBox.Show("Введенные данные некорректны!"); return; }
+                    break;
+
+            }
+            countCells = width * height;
+            minesweeper = new(height, width, mines);
+            DrawField(height, width);
+            MoveToCenterScreen();
+            start.Play();
+            field.Enabled = true;
+            label5.Text = mines.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NewGame.Image = Resources.start_button;
+            if (comboBox1.SelectedIndex == 3) { groupBox2.Visible = true; }
+            else groupBox2.Visible = false;
+        }
+
+        private void ReturnOldSize() 
+        {
+            this.Size = new Size(690, 810);
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            DrawField(9, 9);
+            field.Enabled = false;
         }
     }
 }
